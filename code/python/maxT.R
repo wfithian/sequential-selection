a99ed7cb52068240574cb0c4650b513abbe65f4f
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-07-02 03:29:51 -0700

    added unknown sigma tests, removing maxT function as that is now in the forward_step class

--- code/python/maxT.R
+++ code/python/maxT.R
@@ -1,8 +1,8 @@
 pdf('maxT_view.pdf')
-D1 = read.table('test1_new.csv', sep=',', header=TRUE)
-D2 = read.table('test2_new.csv', sep=',', header=TRUE)
-D3 = read.table('test3_new.csv', sep=',', header=TRUE)
-D4 = read.table('test4_new.csv', sep=',', header=TRUE)
+D1 = read.table('test1.csv', sep=',', header=TRUE)
+D2 = read.table('test2.csv', sep=',', header=TRUE)
+D3 = read.table('test3.csv', sep=',', header=TRUE)
+D4 = read.table('test4.csv', sep=',', header=TRUE)
 D = rbind(D1,D2,D3,D4)
 write.table(D, "test.csv", row.names=FALSE, col.names=TRUE, sep=',')
 
@@ -10,36 +10,78 @@ par(mfrow=c(2,2))
 
 # mostly signal
 
-plot(ecdf(D$maxT_pvalue_4), main='Step 4')
+plot(ecdf(D$maxT_pvalue_4), main='Step 4', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_4), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_5), main='Step 5')
+plot(ecdf(D$maxT_identify_unknown_pvalue_4), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_4), add=TRUE, col='green')
+plot(ecdf(D$saturated_pvalue_4), add=TRUE, col='red')
+
+plot(ecdf(D$maxT_pvalue_5), main='Step 5', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_5), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_6), main='Step 6')
+plot(ecdf(D$saturated_pvalue_5), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_5), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_5), add=TRUE, col='green')
+
+plot(ecdf(D$maxT_pvalue_6), main='Step 6', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_6), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_7), main='Step 7')
+plot(ecdf(D$saturated_pvalue_6), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_6), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_6), add=TRUE, col='green')
+
+plot(ecdf(D$maxT_pvalue_7), main='Step 7', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_7), add=TRUE, col='blue')
+plot(ecdf(D$saturated_pvalue_7), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_7), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_7), add=TRUE, col='green')
 
 # mixed
 
-plot(ecdf(D$maxT_pvalue_8), main='Step 8')
+plot(ecdf(D$maxT_pvalue_8), main='Step 8', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_8), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_9), main='Step 9')
+plot(ecdf(D$saturated_pvalue_8), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_8), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_8), add=TRUE, col='green')
+
+plot(ecdf(D$maxT_pvalue_9), main='Step 9', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_9), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_10), main='Step 10')
+plot(ecdf(D$saturated_pvalue_9), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_9), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_9), add=TRUE, col='green')
+
+plot(ecdf(D$maxT_pvalue_10), main='Step 10', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_10), add=TRUE, col='blue')
-plot(ecdf(D$maxT_pvalue_11), main='Step 11')
+plot(ecdf(D$saturated_pvalue_10), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_10), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_10), add=TRUE, col='green')
+
+plot(ecdf(D$maxT_pvalue_11), main='Step 11', xlim=c(0,1))
 plot(ecdf(D$maxT_identify_pvalue_11), add=TRUE, col='blue')
+plot(ecdf(D$saturated_pvalue_11), add=TRUE, col='red')
+plot(ecdf(D$maxT_identify_unknown_pvalue_11), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_11), add=TRUE, col='green')
 
 # mostly nulls
 
+{ 
 plot(ecdf(D$maxT_pvalue_20), main='Step 20')
 plot(ecdf(D$maxT_identify_pvalue_20), add=TRUE, col='blue')
+plot(ecdf(D$maxT_identify_unknown_pvalue_20), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_20), add=TRUE, col='green')
+
 plot(ecdf(D$maxT_pvalue_21), main='Step 21')
 plot(ecdf(D$maxT_identify_pvalue_21), add=TRUE, col='blue')
+plot(ecdf(D$maxT_identify_unknown_pvalue_21), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_21), add=TRUE, col='green')
+
 plot(ecdf(D$maxT_pvalue_22), main='Step 22')
 plot(ecdf(D$maxT_identify_pvalue_22), add=TRUE, col='blue')
+plot(ecdf(D$maxT_identify_unknown_pvalue_22), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_22), add=TRUE, col='green')
+
 plot(ecdf(D$maxT_pvalue_23), main='Step 23')
 plot(ecdf(D$maxT_identify_pvalue_23), add=TRUE, col='blue')
+plot(ecdf(D$maxT_identify_unknown_pvalue_23), add=TRUE, col='yellow')
+plot(ecdf(D$maxT_unknown_pvalue_23), add=TRUE, col='green')
 
 # later in the path
 
@@ -63,5 +105,6 @@ plot(ecdf(D$maxT_identify_pvalue_38), add=TRUE, col='blue')
 plot(ecdf(D$maxT_pvalue_39), main='Step 39')
 plot(ecdf(D$maxT_identify_pvalue_39), add=TRUE, col='blue')
 
+}
 
 dev.off()
